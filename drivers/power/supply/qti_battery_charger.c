d3e865d51cab29e3e29e66785a1eabc92d482ed8
micky387 <mickaelsaibi@free.fr>
2025-11-16 05:40:47 -0500

    sm8450: Add asus_battery_charger for AI2202 project
    Change-Id: I6273fc50ae6d958e58ec4c934762cbe0a3d55819
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -22,6 +22,7 @@
 #include <linux/reboot.h>
 #include <linux/soc/qcom/pmic_glink.h>
 #include <linux/soc/qcom/battery_charger.h>
+#include <linux/soc/qcom/battery_charger_asus.h>
 #include <linux/soc/qcom/panel_event_notifier.h>
 #include "qti_typec_class.h"
 
@@ -63,12 +64,20 @@ enum usb_connector_type {
 	USB_CONNECTOR_TYPE_MICRO_USB,
 };
 
+#if defined ASUS_AI2202_PROJECT
+extern int asuslib_init(void);
+extern int asuslib_deinit(void);
+extern void asus_monitor_start(int status);
+extern void set_qc_stat(int status);
+#else
+//Move to battery_charger.h
 enum psy_type {
 	PSY_TYPE_BATTERY,
 	PSY_TYPE_USB,
 	PSY_TYPE_WLS,
 	PSY_TYPE_MAX,
 };
+#endif
 
 enum ship_mode_type {
 	SHIP_MODE_PMIC,
@@ -216,6 +225,10 @@ struct battery_charger_ship_mode_req_msg {
 	u32			ship_mode_type;
 };
 
+#if defined ASUS_AI2202_PROJECT
+//ASUS_BSP : Move battery_chg_dev to battery_charger.h
+extern struct battery_chg_dev *g_bcdev;
+#else
 struct psy_state {
 	struct power_supply	*psy;
 	char			*model;
@@ -273,6 +286,7 @@ struct battery_chg_dev {
 	bool				notify_en;
 	bool				error_prop;
 };
+#endif
 
 static const int battery_prop_map[BATT_PROP_MAX] = {
 	[BATT_STATUS]		= POWER_SUPPLY_PROP_STATUS,
@@ -380,8 +394,16 @@ static int battery_chg_fw_write(struct battery_chg_dev *bcdev, void *data,
 	return rc;
 }
 
+#if defined ASUS_AI2202_PROJECT
+//ASUS_BSP : remove the static use of battery_chg_write
+//static int battery_chg_write(struct battery_chg_dev *bcdev, void *data,
+//				int len)
+int battery_chg_write(struct battery_chg_dev *bcdev, void *data,
+				int len)
+#else
 static int battery_chg_write(struct battery_chg_dev *bcdev, void *data,
 				int len)
+#endif
 {
 	int rc;
 
@@ -1102,15 +1124,26 @@ static int usb_psy_set_icl(struct battery_chg_dev *bcdev, u32 prop_id, int val)
 		return rc;
 	}
 
-	/* Allow this only for SDP, CDP or USB_PD and not for other charger types */
-	switch (pst->prop[USB_ADAP_TYPE]) {
-	case POWER_SUPPLY_USB_TYPE_SDP:
-	case POWER_SUPPLY_USB_TYPE_PD:
-	case POWER_SUPPLY_USB_TYPE_CDP:
-		break;
-	default:
+	//[+++]ASUS_BSP : ONLY allow SDP to set ICL
+#if 0
+	/* Allow this only for SDP or USB_PD and not for other charger types */
+	if (pst->prop[USB_ADAP_TYPE] != POWER_SUPPLY_USB_TYPE_SDP &&
+	    pst->prop[USB_ADAP_TYPE] != POWER_SUPPLY_USB_TYPE_PD)
+		return -EINVAL;
+#endif
+	/* Allow this only for SDP and not for other charger types */
+	if (pst->prop[USB_ADAP_TYPE] != POWER_SUPPLY_USB_TYPE_SDP) {
+		pr_debug("Not SDP. skip to set USB ICL. tpye : %d\n", pst->prop[USB_ADAP_TYPE]);
 		return -EINVAL;
 	}
+	//[---]ASUS_BSP : ONLY allow SDP to set ICL
+
+	//[+++] ASUS_BSP : Skip to set ICL=2mA to avoid USBIN suspend
+		if (val < 100000) {
+			pr_err("USB Input Current Limit less than 100ma ,so skip to set icl to avoid USBIN suspend\n");
+			return 0;
+		}
+	//[---] ASUS_BSP : Skip to set ICL=2mA to avoid USBIN suspend
 
 	/*
 	 * Input current limit (ICL) can be set by different clients. E.g. USB
@@ -1152,9 +1185,18 @@ static int usb_psy_get_prop(struct power_supply *psy,
 	if (rc < 0)
 		return rc;
 
+#if defined ASUS_AI2202_PROJECT
+	pval->intval = pst->prop[prop_id];
+	if (prop == POWER_SUPPLY_PROP_TEMP){
+		pval->intval = DIV_ROUND_CLOSEST((int)pval->intval, 10);
+	}else if(prop == POWER_SUPPLY_PROP_ONLINE){
+		asus_monitor_start(pval->intval);
+	}
+#else
 	pval->intval = pst->prop[prop_id];
 	if (prop == POWER_SUPPLY_PROP_TEMP)
 		pval->intval = DIV_ROUND_CLOSEST((int)pval->intval, 10);
+#endif
 
 	return 0;
 }
@@ -1333,6 +1375,12 @@ static int battery_psy_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT_MAX:
 		pval->intval = bcdev->num_thermal_levels;
 		break;
+#if defined ASUS_AI2202_PROJECT
+	case POWER_SUPPLY_PROP_STATUS:
+		pval->intval = pst->prop[prop_id];
+		set_qc_stat(pval->intval);
+		break;
+#endif
 	default:
 		pval->intval = pst->prop[prop_id];
 		break;
@@ -2532,6 +2580,13 @@ static int battery_chg_probe(struct platform_device *pdev)
 
 	schedule_work(&bcdev->usb_type_work);
 
+#if defined ASUS_AI2202_PROJECT
+	//[+++]ASUS_BSP : Add for OEM sub-function
+	g_bcdev = bcdev;
+	asuslib_init();
+	//[+++]ASUS_BSP : Add for OEM sub-function
+#endif
+
 	return 0;
 error:
 	down_write(&bcdev->state_sem);
@@ -2573,6 +2628,10 @@ static int battery_chg_remove(struct platform_device *pdev)
 	cancel_work_sync(&bcdev->battery_check_work);
 	unregister_reboot_notifier(&bcdev->reboot_notifier);
 
+#if defined ASUS_AI2202_PROJECT
+	asuslib_deinit();//ASUS_BSP : Add for sub-function
+#endif
+
 	return 0;
 }
 
@@ -2581,10 +2640,19 @@ static const struct of_device_id battery_chg_match_table[] = {
 	{},
 };
 
+#if defined ASUS_AI2202_PROJECT
+static const struct dev_pm_ops asus_chg_pm_ops = {
+	.resume		= asus_chg_resume,
+};
+#endif
+
 static struct platform_driver battery_chg_driver = {
 	.driver = {
 		.name = "qti_battery_charger",
 		.of_match_table = battery_chg_match_table,
+#if defined ASUS_AI2202_PROJECT
+		.pm	= &asus_chg_pm_ops,
+#endif
 	},
 	.probe = battery_chg_probe,
 	.remove = battery_chg_remove,
--- include/linux/soc/qcom/battery_charger_asus.h
+++ include/linux/soc/qcom/battery_charger_asus.h
@@ -5,7 +5,6 @@
 #ifndef _BATTERY_CHARGER_ASUS_H
 #define _BATTERY_CHARGER_ASUS_H
 
-
 #include <linux/device.h>
 #include <linux/power_supply.h>
 
@@ -13,5 +12,75 @@
 #define QTI_POWER_SUPPLY_UNCHARGED 0x0002
 extern void qti_charge_register_notify(struct notifier_block *nb);
 extern void qti_charge_unregister_notify(struct notifier_block *nb);
+
+struct psy_state {
+	struct power_supply	*psy;
+	char			*model;
+	const int		*map;
+	u32			*prop;
+	u32			prop_count;
+	u32			opcode_get;
+	u32			opcode_set;
+};
+
+enum psy_type {
+	PSY_TYPE_BATTERY,
+	PSY_TYPE_USB,
+	PSY_TYPE_WLS,
+	PSY_TYPE_MAX,
+};
+
+struct battery_chg_dev {
+	struct device			*dev;
+	struct class			battery_class;
+	struct pmic_glink_client	*client;
+	struct typec_role_class		*typec_class;
+	struct mutex			rw_lock;
+	struct rw_semaphore		state_sem;
+	struct completion		ack;
+	struct completion		fw_buf_ack;
+	struct completion		fw_update_ack;
+	struct psy_state		psy_list[PSY_TYPE_MAX];
+	struct dentry			*debugfs_dir;
+	void				*notifier_cookie;
+	/* extcon for VBUS/ID notification for USB for micro USB */
+	struct extcon_dev		*extcon;
+	u32				*thermal_levels;
+	const char			*wls_fw_name;
+	int				curr_thermal_level;
+	int				num_thermal_levels;
+	int				shutdown_volt_mv;
+	atomic_t			state;
+	struct work_struct		subsys_up_work;
+	struct work_struct		usb_type_work;
+	struct work_struct		battery_check_work;
+	int				fake_soc;
+	bool				block_tx;
+	bool				ship_mode_en;
+	bool				debug_battery_detected;
+	bool				wls_not_supported;
+	bool				wls_fw_update_reqd;
+	u32				wls_fw_version;
+	u16				wls_fw_crc;
+	u32				wls_fw_update_time_ms;
+	struct notifier_block		reboot_notifier;
+	u32				thermal_fcc_ua;
+	u32				restrict_fcc_ua;
+	u32				last_fcc_ua;
+	u32				usb_icl_ua;
+	u32				thermal_fcc_step;
+	u32				connector_type;
+	u32				usb_prev_mode;
+	bool				restrict_chg_en;
+	/* To track the driver initialization status */
+	bool				initialized;
+	bool				notify_en;
+	bool				error_prop;
+};
+
+int asuslib_init(void);
+int asuslib_deinit(void);
+int asus_chg_resume(struct device *dev);
+void set_qc_stat(int status);
 
 #endif
